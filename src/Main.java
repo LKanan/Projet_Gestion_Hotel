ec26612c2305c7f827e3076ee5762abcf0f860ba
LKanan <lucienkanani@gmail.com>
2025-04-04 16:04:52 +0200

    Refactor chambre methods in Main.java for clarity.
    Simplified the main method by calling broader, encapsulated methods in the Chambre class. Removed repetitive code for chamber addition and display, and added a delete operation for better functionality testing.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String [] args){
-        Chambre.addChamber("Chambre 1", 100, "Description 1", "Type 1");
-        Chambre.addChamber("Chambre 2", 200, "Description 2", "Type 2");
-        Chambre.addChamber("Chambre 3", 300, "Description 3", "Type 3");
-        for(int i = 0; i< Chambre.chambres.size(); i++){
-            System.out.println(Chambre.chambres.get(i).toString());
-        }
+        Chambre.addChamber();
+        Chambre.afficherChambres();
+        Chambre.supprimerChamber();
+        Chambre.afficherChambres();
     }
 }
